f96856deca3489781e557e989006f1f655f8d860
unknown <fabionicollassilva@gmail.com>
2023-08-23 14:16:55 -0300

    Enviando projeto para deploy

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Frase from './components/Frase'
+import List from './components/List'
+
+function App() {
+  return (
+    <>
+      <Frase/>
+      <Frase/>
+      <List/>
+    </>
+  )
+}
+
+export default App
